4150d38a8dd3f02fbae90bdad98402ea1089146f
KrivosheyYuriy <lonespy@yandex.ru>
2023-02-16 16:37:21 +0300

    2+3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for, render_template
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
 
@@ -6,7 +7,18 @@ app = Flask(__name__)
 @app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
-    return render_template('base.html', title='Колонизация')
+    return render_template('training.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('base.html', prof=prof.lower())
+
+
+@app.route('/list_prof/<lst>')
+def list_prof(lst):
+    professions = ['Пилот', 'Штурман', 'Строитель', 'ученый', 'инженер']
+    return render_template('list_prof.html', lst=lst, professions=professions)
 
 
 if __name__ == '__main__':
